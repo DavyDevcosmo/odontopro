5fb1612ccbcf70a5690946e0366252477824f606
Davy cosmo <davydevcosmo@gmail.com>
2025-09-15 05:32:00 -0300

    style:  adicionado responsividade

--- src/app/(public)/_components/header.tsx
+++ src/app/(public)/_components/header.tsx
@@ -50,7 +50,7 @@ Portal da clinica
 
   return (
    <header className="fixed top-0 right-0 left-0 z-{999} py-4 px-6 bg-white ">
-    <div className="container mx-auto flex items-center justify-between ">
+    <div className="container mx-auto flex items-center pb-4 sm:pb-0 justify-between ">
 
       <Link href="/" className="text-3xl font-bold text-shadow-zinc-900">
       Odonto<span className="text-emerald-500">Pro</span>
