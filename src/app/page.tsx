cf9292059f14de7232dce958ae0c0796f4d037f3
Davy cosmo <davydevcosmo@gmail.com>
2025-09-16 04:48:32 -0300

    imports: correção de imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
+import { Footer } from "./(public)/_components/footer";
 import Header from "./(public)/_components/header";
-import { Hero } from "./(public)/_components/Hero";
+import { Hero } from "./(public)/_components/hero";
+import { Professionals } from "./(public)/_components/professionals";
+
+
 
 export default function Home() {
   return(
@@ -8,6 +12,8 @@ export default function Home() {
 
       <div>
        <Hero/>
+       <Professionals/>
+       <Footer/>
       </div>
     </div> 
   )
