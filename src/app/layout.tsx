bc2b90d44b0fc6da59100a0f8aca9bafb487fa9b
Davy cosmo <davydevcosmo@gmail.com>
2025-10-06 19:45:36 -0300

    feat: SessionAuthProvider inplementado

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
+
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import SessionAuthProvider from "@/components/ui/session-auth";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +29,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <SessionAuthProvider>
+          {children}
+        </SessionAuthProvider>
       </body>
     </html>
   );
