a05a0f9ce754a410a6ef12c10afc43e377383cc0
Davy cosmo <davydevcosmo@gmail.com>
2025-10-21 23:19:33 -0300

    feat: redireciona o usuario quando não esta logado

--- src/app/(panel)/Dashboard/page.tsx
+++ src/app/(panel)/Dashboard/page.tsx
@@ -1,5 +1,14 @@
-"use client"
-export default function Dashboard() {
+import getSesion from "../../../lib/getSession"
+import { redirect } from "next/navigation"
+
+export default async function Dashboard() {
+    const session = await getSesion()
+
+
+    if (!session) {
+        redirect("/")
+    }
+
     return (
         <div>
             <h1>hello dashboard</h1>
