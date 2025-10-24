576df29ec021ff6073b8335b03c2e5ba5c9324bf
Davy cosmo <davydevcosmo@gmail.com>
2025-10-23 23:26:32 -0300

    feat: controle se sessão

--- src/app/(panel)/Dashboard/profile/page.tsx
+++ src/app/(panel)/Dashboard/profile/page.tsx
@@ -1,8 +1,26 @@
-"use client"
-export default function Profile() {
+
+import { redirect } from "next/navigation"
+import { getUserData } from "./_data-access/get-info-user"
+import getSession from "@/lib/getSession"
+import ProfileContent from "./_componets/profile"
+
+
+export default async function Profile() {
+    const session = await getSession()
+
+    if (!session) {
+        redirect("/")
+    }
+
+    const user = await getUserData({ userId: session.user?.id })
+    console.log("getUserData: ", user)
+
+    if (!user) {
+        redirect("/")
+    }
     return (
         <div>
-            <h1>profile tese</h1>
+            <ProfileContent />
         </div>
     )
 }
