5bb26b1b0d14d473d5d7aee3aeec8362fdf10593
Davy cosmo <davydevcosmo@gmail.com>
2025-11-10 11:22:41 -0300

    fix: disponibilizando o Toaster para todo o projeto

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { SessionAuthProvider } from "@/components/session-auth";
+import { Toaster } from 'sonner'
 
 
 const geistSans = Geist({
@@ -31,6 +32,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <SessionAuthProvider>
+          <Toaster duration={2500} />
           {children}
         </SessionAuthProvider>
       </body>
