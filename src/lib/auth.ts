ed3e98b361b6bc227409d673807e27ec3cd5b321
Davy cosmo <davydevcosmo@gmail.com>
2025-11-17 04:45:57 -0300

    fix: aficia o trustHost: true

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -8,4 +8,5 @@ import GitHub from "next-auth/providers/github"
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma) as Adapter,
   providers: [GitHub],
+  trustHost: true,
 })
