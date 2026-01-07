91194005fb2c65cf63346b4edae093bb50ab2623
Davy cosmo <davydevcosmo@gmail.com>
2026-01-07 06:08:12 -0300

    feat: cria componente Home com estrutura básica e importações necessárias

---
+++ src/app/(public)/page.tsx
@@ -0,0 +1,22 @@
+"use client"
+
+import { Footer } from "./_components/Footer";
+import Header from "./_components/Header";
+import { Hero } from "./_components/Hero";
+import { Professionals } from "./_components/Professionals";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Header />
+
+      <div>
+        <Hero />
+
+        <Professionals />
+
+        <Footer />
+      </div>
+    </div>
+  )
+}
