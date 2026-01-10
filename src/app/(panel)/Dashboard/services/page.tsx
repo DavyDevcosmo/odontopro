60ec2ffd7792af9f9ba2607174145e9c57b57e9b
Davy cosmo <davydevcosmo@gmail.com>
2026-01-10 20:39:11 -0300

    feat: atualiza componente services para redirecionar usuários não autenticados e renderizar conteúdo de serviços

--- src/app/(panel)/Dashboard/services/page.tsx
+++ src/app/(panel)/Dashboard/services/page.tsx
@@ -1,8 +1,16 @@
-"use client"
-export default function Serveces() {
+
+import { redirect } from "next/navigation"
+import { ServicesContent } from "./_componets/services-content"
+import getSession from "@/lib/getSession"
+
+export default async function services() {
+    const session = await getSession()
+
+    if (!session) {
+        redirect("/")
+    }
+
     return (
-        <div>
-            <h1>services </h1>
-        </div>
+        <ServicesContent userId={session.user?.id!} />
     )
 }
