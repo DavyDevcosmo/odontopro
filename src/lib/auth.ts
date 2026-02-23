17f60a86912c9dc0b73e4c2941880b742d825798
Davy cosmo <davydevcosmo@gmail.com>
2026-02-23 20:05:13 -0300

    imports: correção de imports

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,12 +1,12 @@
 import NextAuth from "next-auth"
+
 import { PrismaAdapter } from "@auth/prisma-adapter"
-import prisma from "./prisma"
 import { Adapter } from "next-auth/adapters"
 import GitHub from "next-auth/providers/github"
+import prisma from "./prisma"
 
- 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma) as Adapter,
-  providers: [GitHub],
   trustHost: true,
+  providers: [GitHub],
 })
