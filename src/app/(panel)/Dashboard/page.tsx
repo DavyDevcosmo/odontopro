9be9afccf58189372ee8a6266fa938ee1e7f61e4
Davy cosmo <davydevcosmo@gmail.com>
2026-04-22 20:14:30 -0300

    feat: Appointments Mostra os todos os agendamentos disponivel. Reminders cria lembretes

--- src/app/(panel)/Dashboard/page.tsx
+++ src/app/(panel)/Dashboard/page.tsx
@@ -1,5 +1,12 @@
+import Link from "next/link"
 import getSesion from "../../../lib/getSession"
 import { redirect } from "next/navigation"
+import { Button } from "@/components/ui/button"
+import { Calendar } from "lucide-react"
+import { ButtonCopyLink } from "./_components/button-copy-link"
+import { Reminders } from "./_components/reminders/reminders"
+import { Appointments } from "./_components/appointments/appointments"
+
 
 export default async function Dashboard() {
     const session = await getSesion()
@@ -10,8 +17,27 @@ export default async function Dashboard() {
     }
 
     return (
-        <div>
-            <h1>hello dashboard</h1>
-        </div>
+        <main>
+            <div className="space-x-2 flex items-center justify-end">
+                <Link href={`/clinica/${session.user?.id}`}
+                    target="_black">
+                    <Button className="bg-emerald-500 hover:bg-emerald-400 flex-1 md:flex-[0]">
+                        <Calendar className="w-5 h-5 " />
+                        <span>Novo agendamento</span>
+                    </Button>
+                </Link>
+
+                <ButtonCopyLink userId={session.user?.id!} />
+            </div>
+
+            <section className="grid grid-cols1 gap-4 lg:grid-cols-2 mt-4">
+                <div>Agenda</div>
+
+                <Appointments userId={session.user?.id!} />
+
+                <Reminders userId={session.user?.id!} />
+
+            </section>
+        </main>
     )
 }
