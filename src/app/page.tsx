73453e2589586b1702a1f41d0171107d3717ab97
Davy cosmo <davydevcosmo@gmail.com>
2026-05-04 20:45:29 -0300

    fix: renderiza os profissionais na home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,21 @@
-"use client"
 
 import { Footer } from "./(public)/_components/Footer"
 import Header from "./(public)/_components/Header"
 import { Hero } from "./(public)/_components/Hero"
 import { Professionals } from "./(public)/_components/Professionals";
+import { getProfessionals } from "./(public)/data-acess/get-professional";
 
+export const revalidate = 120;
 
+export default async function Home() {
+  const professionals = await getProfessionals();
 
-export default function Home() {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
-
       <div>
         <Hero />
-        <Professionals />
+        <Professionals professionals={professionals} />
         <Footer />
       </div>
     </div>
