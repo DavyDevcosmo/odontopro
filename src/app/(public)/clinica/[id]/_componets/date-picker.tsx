8abb2bf1cd097eaf76c3a4bef0d6f51520ec4fdc
Davy cosmo <davydevcosmo@gmail.com>
2026-05-04 20:47:56 -0300

    fix: seleciona a data do agendamento e formata

--- src/app/(public)/clinica/[id]/_componets/date-picker.tsx
+++ src/app/(public)/clinica/[id]/_componets/date-picker.tsx
@@ -1,9 +1,9 @@
 "use client"
-import DatePicker, { registerLocale } from "react-datepicker";
-import { ptBR } from 'date-fns/locale/pt-BR';
-import { useState } from "react";
+import { useState } from 'react'
+import DatePicker, { registerLocale } from 'react-datepicker'
+import { ptBR } from 'date-fns/locale/pt-BR'
 
-import "react-datepicker/dist/react-datepicker.css";
+import "react-datepicker/dist/react-datepicker.css"
 
 registerLocale("pt-BR", ptBR)
 
@@ -17,14 +17,14 @@ interface DateTimePickerProps {
 export function DateTimePicker({ initialDate, className, minDate, onChange }: DateTimePickerProps) {
     const [startDate, setStartDate] = useState(initialDate || new Date())
 
-
     function handleChange(date: Date | null) {
         if (date) {
             console.log(date);
             setStartDate(date);
-            onChange(date);
+            onChange(date)
         }
     }
+
 
     return (
         <DatePicker
