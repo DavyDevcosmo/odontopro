39dec8ac8aa1480c3cd781b80ec96a67d65b0e67
Davy cosmo <davydevcosmo@gmail.com>
2026-05-18 05:25:30 -0300

    Feat: card de gerenciamento de assinatura

---
+++ src/app/(panel)/Dashboard/plans/components/subscription-detail.tsx
@@ -0,0 +1,62 @@
+"use client"
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { subscriptionPlans } from "@/utils/plans";
+import { Subscription } from "@prisma/client";
+import { createPortalCustomer } from "../_actions/create-portal-customer";
+import { toast } from "sonner";
+import { Button } from "@/components/ui/button";
+
+
+interface SubscriptionDetailProps {
+    subscription: Subscription;
+}
+
+export function SubscriptionDetail({ subscription }:
+    SubscriptionDetailProps) {
+
+    const subscriptionInfo = subscriptionPlans.find(plan => plan.id === subscription.plan)
+
+    async function handleManegeSubscription() {
+        const portal = await createPortalCustomer()
+
+        if (portal.error) {
+            toast.error("Ocorreu um erro ao criar o portal de assinatura")
+        }
+
+        window.location.href = portal.sessionId;
+
+    }
+    return (
+        <Card className="w-full mx-auto">
+            <CardHeader>
+                <CardTitle className="text-2xl">Seu Plano Atual</CardTitle>
+                <CardDescription>Sua assinatura está ativa!
+                </CardDescription>
+            </CardHeader>
+
+            <CardContent>
+                <div className="flex items-center justify-between">
+                    <h3 className="font-semibold text-lg md:text-xl">
+                        {subscription.plan === "BASIC" ? "BASIC" : "PROFESSIONAL"}
+                    </h3>
+
+                    <div className="bg-green-500 text-white w-fit px-4 py-1 rounded-md">
+                        {subscription.status === "active" ? "ATIVO" : "INATIVO"}
+                    </div>
+                </div>
+
+                <ul className="list-disc list-inside space-y-2">
+                    {subscriptionInfo && subscriptionInfo.features.map(features => (
+                        <li key={features}>{features}</li>
+                    ))}
+                </ul>
+            </CardContent>
+            <CardFooter>
+                <Button onClick={handleManegeSubscription} className="bg-black text-white">Gerenciar assinatura</Button>
+
+            </CardFooter>
+
+
+        </Card>
+    )
+}
