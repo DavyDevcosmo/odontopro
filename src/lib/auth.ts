b4584aa59e188c937cb7c4aeed8915b9de215427
Davy cosmo <davydevcosmo@gmail.com>
2026-06-04 22:52:24 -0300

    fix: login com google

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -3,10 +3,11 @@ import NextAuth from "next-auth"
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import { Adapter } from "next-auth/adapters"
 import GitHub from "next-auth/providers/github"
+import Google from "next-auth/providers/google"
 import prisma from "./prisma"
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma) as Adapter,
   trustHost: true,
-  providers: [GitHub],
+  providers: [GitHub, Google],
 })
