d2e29c661d0214350f0b912cd3f5fc76baae693e
Davy cosmo <davydevcosmo@gmail.com>
2026-06-23 20:37:08 -0300

    feat: verifica a sessão do usuario e com base se assinatura esta ativa mostra o cards de planos

--- src/app/(panel)/Dashboard/plans/page.tsx
+++ src/app/(panel)/Dashboard/plans/page.tsx
@@ -1,8 +1,28 @@
-"use client"
-export default function Plans() {
+import { GridPlans } from "./components/grid-plans"
+import { redirect } from "next/navigation"
+import { getSubscription } from "@/utils/get-subscription"
+import { auth } from "@/lib/auth"
+import { SubscriptionDetail } from "./components/subscription-detail"
+
+export default async function Plans() {
+
+    const session = await auth()
+
+    if (!session) {
+        redirect("/")
+    }
+
+    const subscritpion = await getSubscription({ userId: session?.user?.id! })
+
     return (
         <div>
-            <h1>profile tese</h1>
+            {subscritpion?.status !== "active" && (
+                <GridPlans />
+            )}
+
+            {subscritpion?.status === "active" && (
+                <SubscriptionDetail subscription={subscritpion} />
+            )}
         </div>
     )
 }
